ab46f3470d9282fea1fb30da1cc802a741d45be4
wangan <wangan16@163.com>
2020-12-26 16:04:34 +0800

    xy

--- copytoclp.js
+++ copytoclp.js
@@ -1,19 +1,35 @@
 /**  * 复制内容到粘贴板  *
  *  content : 需要复制的内容  *
  * message : 复制完后的提示，不传则默认提示"复制成功"  */
-function copyToClip(content, message) {
+function copyToClip(content) {
   var aux = document.createElement("input");
   aux.setAttribute("value", content);
   document.body.appendChild(aux);
   aux.select();
   document.execCommand("copy");
   document.body.removeChild(aux);
-  var a=1;
-  if (message == null) {
-    //alert("复制成功");
-    console.log("复制成功");
-  } else {
-    //alert(message);
-    console.log(message);
+  var a = 1;
+
+  console.log("复制成功", content);
+}
+function copyX(param) {
+  conck(param, "x");
+}
+function copyY(param) {
+  conck(param, "y");
+}
+function conck(param, Axis) {
+  // console.log("🚀 ~ file: copytoclp.js ~ line 22 ~ conck ~ Axis", Axis);
+  console.log(param);
+  var datax = param.name;
+  // console.log("🚀 ~ file: copy.html ~ line 45 ~ conck ~ datax", datax);
+  var datay = param.data;
+  // console.log("🚀 ~ file: copy.html ~ line 46 ~ conck ~ datay", datay);
+  if (Axis == "x") {
+    copyToClip(datax);
+  } else if (Axis == "y") {
+    copyToClip(datay);
   }
+
+  // alert(datay);
 }
